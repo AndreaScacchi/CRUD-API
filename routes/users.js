1b73cd743b134403cf08efdb8939e8ffb6d16f9c
AndreaScacchi <andreascacchi10@gmail.com>
2022-04-28 17:06:27 +0200

    new folder and file added and code refactored

--- routes/users.js
+++ routes/users.js
@@ -1,52 +1,21 @@
 import express from 'express';
-import { v4 as uuidv4 } from 'uuid';
+import { createUser, getUsers, getUser, deleteUser, updateUser } from "../controllers/users.js";
 
 const router = express.Router();
 
-let users = [];
-
 // all routes in here are starting with /users
-router.get('/', (req, res) => {
-    res.send(users);
-});
+router.get('/', getUsers);
 
 // push users to the database
-router.post('/', (req, res) => {
-    const user = req.body;
-
-    // spread all the properties of the user plus create an ID
-    users.push({ ...user, id: uuidv4() });
-    res.send(`User with the name ${user.firstName} added to the database!`);
-});
+router.post('/', createUser);
 
 // new route for different users
-router.get('/:id', (req, res) => {
-    const { id } = req.params;
-
-    // send the user data for a specific id
-    const foundUser = users.find((user) => user.id === id);
-
-    res.send(foundUser);
-});
+router.get('/:id', getUser);
 
 // new route for delete users
-router.delete('/:id', (req, res) => {
-    const { id } = req.params;
-    users = users.filter((user) => user.id !== id);
-    res.send(`User with the id ${id} deleted from the database!`);
-});
+router.delete('/:id', deleteUser);
 
 // new route for change user details
-router.patch('/:id', (req, res) => {
-    const { id } = req.params;
-    const { firstName, lastName, age} = req.body;
-    const user = users.find((user) => user.id === id);
-
-    if(firstName) user.firstName = firstName;
-    if (lastName) user.lastName = lastName;
-    if (age) user.age = age;
-
-    res.send(`User witht the id ${id} has been updated`);
-})
+router.patch('/:id', updateUser);
 
 export default router;
