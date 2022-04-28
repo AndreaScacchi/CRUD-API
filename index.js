cd3943ac25663902528a660a7f37873b2cf81f2a
AndreaScacchi <andreascacchi10@gmail.com>
2022-04-28 12:47:24 +0200

    bodyparser initialized and server created

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ import bodyParser from 'body-parser';
 
 const app = express();
 const PORT = 5000;
+
+app.use(bodyParser.json());
+
+app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
