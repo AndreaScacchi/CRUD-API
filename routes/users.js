5525412c769fe34539ba410308a8d02ee18b4e1e
AndreaScacchi <andreascacchi10@gmail.com>
2022-04-28 16:47:51 +0200

    all routes implemented

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
-const users = [];
+let users = [];
 
 // all routes in here are starting with /users
 router.get('/', (req, res) => {
@@ -28,5 +28,25 @@ router.get('/:id', (req, res) => {
 
     res.send(foundUser);
 });
+
+// new route for delete users
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    users = users.filter((user) => user.id !== id);
+    res.send(`User with the id ${id} deleted from the database!`);
+});
+
+// new route for change user details
+router.patch('/:id', (req, res) => {
+    const { id } = req.params;
+    const { firstName, lastName, age} = req.body;
+    const user = users.find((user) => user.id === id);
+
+    if(firstName) user.firstName = firstName;
+    if (lastName) user.lastName = lastName;
+    if (age) user.age = age;
+
+    res.send(`User witht the id ${id} has been updated`);
+})
 
 export default router;
