550f07585857a55b50b2ebbbeaa715fe50d69bb8
AndreaScacchi <andreascacchi10@gmail.com>
2022-04-28 15:39:14 +0200

    new functionality added to push new users with unique IDs in the database

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,9 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
+
 const router = express.Router();
 
-const users = [
-    {
-        firstName: "John",
-        lastName: "Doe",
-        age: 25,
-    },
-
-    {
-        firstName: "Jane",
-        lastName: "Doe",
-        age: 24,
-    },
-];
+const users = [];
 
 // all routes in here are starting with /users
 router.get('/', (req, res) => {
@@ -23,7 +13,10 @@ router.get('/', (req, res) => {
 // push users to the database
 router.post('/', (req, res) => {
     const user = req.body;
-    users.push(user);
+
+    // create a new object which represents the user and then spread all the properties of the user
+    // plus create an ID
+    users.push({ ...user, id: uuidv4() });
     res.send(`User with the name ${user.firstName} added to the database!`);
 });
 
